1476401f802d13525098c0985776f304cd9cb23f
himanshumishra3112 <himanshumishra3112@gmail.com>
2017-08-11 11:24:39 +0530

    shared_ptr

--- sharedPtr.cpp
+++ sharedPtr.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include <memory>
 #include <vector>
+using namespace std;
+
 int main() {
+#if 0
 	boost::shared_ptr<int> p1;
 	std::vector<int> obj (10);
+#endif
+
+	shared_ptr<int> p1(new int(10));
+	cout << "*p1: " << *p1 << endl;
+
+	shared_ptr<int> p2(move(p1));
+	//shared_ptr<int> p2(move(p1)); //behave like unique ptr
+	cout << "*p2: " << *p2 << endl;
+
+	if (p1 != nullptr)
+		cout << "*p1: " << *p1 << endl;
+	else
+		cout << "p1 is now nullptr" << endl;
+
+	return 0;
 }
