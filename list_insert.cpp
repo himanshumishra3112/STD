1da40715fb4ebb274a5f1e37f384da65723e50f9
himanshumishra3112 <himanshumishra3112@gmail.com>
2017-09-01 23:30:43 +0530

    updated

--- list_insert.cpp
+++ list_insert.cpp
@@ -25,7 +25,7 @@ int main()
 		list<Test> t1;
 		{
 			Test* p1 = new Test(10);
-			t1.push_back(*p1);
+			t1.push_back(*p1);				//Call to copy Ctor
 			delete p1;
 		}
 
@@ -49,7 +49,25 @@ int main()
 			cout << (*it)->_a << endl;					//20 30
 			cout << (*it).use_count() << endl;			//1
 		}
+	}
 
+	{
+		list<Test*> t1;
+		{
+			Test* p1 = new Test(40);
+			Test* p2 = new Test(50);
+			t1.push_back(p1);
+			t1.push_back(p2);
+			delete p1;
+			delete p2;
+			p1=nullptr;
+			p2=nullptr;
+		}
+		cout << "size: " << t1.size() << endl;				//2
+		for (auto it = t1.begin(); it != t1.end(); it++)
+		{
+			cout << (*it)->_a << endl;						//garbage values
+		}
 	}
 	return 0;
 }
