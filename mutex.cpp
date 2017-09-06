38a4c2e429eb6a6aec8457b36ab8c245de5cb1cf
himanshumishra3112 <himanshumishra3112@gmail.com>
2017-09-06 21:19:46 +0530

    thread with dedlock issue

---
+++ mutex.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+using namespace std;
+
+struct Complex {
+	std::mutex mutex;
+	int i;
+
+	Complex() : i(10) {}
+
+	void mul(int x){
+		std::lock_guard<std::mutex> lock(mutex);
+		i *= x;
+		cout << i << endl;
+	}
+
+	void div(int x){
+		std::lock_guard<std::mutex> lock(mutex);
+		i /= x;
+		cout << i << endl;
+	}
+
+	void both(int x, int y){
+		std::lock_guard<std::mutex> lock(mutex);
+		mul(x);
+		div(y);
+	}
+};
+
+
+void f(int i)
+{
+	Complex complex;
+	complex.both(10, 10);
+}
+
+
+int main(){
+	std::thread first(f,1);
+
+	first.join();
+	return 0;
+}
