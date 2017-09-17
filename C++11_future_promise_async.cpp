9d5121ae48217f962593decc6ffd335412112cdb
himanshumishra3112 <himanshumishra3112@gmail.com>
2017-09-17 23:45:07 +0530

    future, promise and async

--- C++11_future_promise_async.cpp
+++ C++11_future_promise_async.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 #include <future>
+#include <chrono>
 using namespace std;
 
-int factorial(std::future<int>& N) 
-{ 
-    int res = 1; 
-    int N = f.get(); 
-    for (int i=N; i>1; 1--) 
-        res *= i; 
-    cout « "Result is: " « res « erdl ; 
+int factorial(std::future<int>& f)
+{
+    int res = 1;
+    int N = f.get();
+    for (int i=N; i>1; i--)
+        res *= i;
+    cout << "Result is: " << res << endl;
     return res;
 }
 
-int main() 
-{ 
-    int x; 
-    std::promise<int> p; 
-    std::future<int> f = p.get_future(); 
-    std::future<int> fu = std::async(std::launch::async, factorial, std::ref(f)); 
-    
-    // do something else 
-    std::this_thread::sleep_for(chrono:milliseconds(20)); p.set_value(14); I 
-    x = fu.get(); 
-    cout « "Get from child: " « x « endl;
+int main()
+{
+    int x;
+    std::promise<int> p;
+    std::future<int> f = p.get_future();
+    std::future<int> fu = std::async(std::launch::async, factorial, std::ref(f));
+
+    // do something else
+    std::this_thread::sleep_for(chrono::milliseconds(20));
+	p.set_value(4);
+    x = fu.get();
+    cout << "Get from child: " << x << endl;
     return 0;
 }
